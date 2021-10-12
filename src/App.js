557a62cb6e08918634a8ec67d2a69e2b023db55c
FestiveKyle <kyle.j.s.sullivan@gmail.com>
2021-10-12 19:32:55 -0300

    Add colorscheme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-import { ChakraProvider, Text } from '@chakra-ui/react'
+import { ChakraProvider, Flex, Text } from '@chakra-ui/react'
+import { theme } from './theme'
 
 const App = () => {
   return (
-    <ChakraProvider>
-      <Text fontWeight="bold">Welcome to Debt!</Text>
+    <ChakraProvider theme={theme}>
+      <Flex direction="column" minHeight="100vh" bg="gray.100">
+        <Text color="cyan">WOOOOOO</Text>
+      </Flex>
     </ChakraProvider>
   )
 }
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    darkpurple: '#1F2041',
+    lightpurple: '#4B3F72',
+    yellow: '#FFC857',
+    cyan: '#119DA4',
+    blue: '#19647E',
+  },
+})
