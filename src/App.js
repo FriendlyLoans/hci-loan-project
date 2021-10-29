4f890d108195ba96ca28757d9818fdbf3cb026a5
AJ <55856227+monkeypunk44@users.noreply.github.com>
2021-10-29 19:04:28 -0300

    Mainlayout done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   Text,
   Box,
   Link,
+  Button,
 } from '@chakra-ui/react'
 
 import {
@@ -28,25 +29,55 @@ const App = () => {
           maxW={{ sm: 540, md: 768, lg: 960, xl: 1200 }}
           mx="auto"
           w="100%"
-          bg="blue"
-          minH="100vh"
+          bg="white"
+          minH="95vh"
           flexDirection="column"
         >
+          <Flex
+            bg="blue"
+            alignContent="center"
+            justifyContent="space-between"
+            height="20"
+          >
+            <Heading color="yellow" bg="blue" my="auto">
+              Friendly Loans
+            </Heading>
+            <Button
+              outlineColor="lightpurple"
+              textColor="black"
+              w="24%"
+              my="auto"
+              height="10"
+              bg="cyan"
+              _hover={{ bg: 'lightpurple' }}
+            >
+              Login/Create Account
+            </Button>
+          </Flex>
           <Navigation />
 
-          <Heading color="yellow" my="0.5rem">
-            Friendly Loans
-          </Heading>
           <Divider orientation="horizontal" />
 
           <Box mx="auto">
             <Switch>
-              <Route path="/history">History</Route>
+              <Route path="/account">Account</Route>
               <Route path="/contact">Contact</Route>
               <Route path="/faq">FAQ</Route>
               <Route path="/">Home</Route>
+              <Route path="/about">About</Route>
             </Switch>
           </Box>
+        </Flex>
+        <Flex
+          maxW={{ sm: 540, md: 768, lg: 960, xl: 1200 }}
+          mx="auto"
+          w="100%"
+          bg="blue"
+          bg="blue"
+          minH="5vh"
+          textColor="yellow"
+        >
+          <Text m="auto">blabla bla idk copyright 1990 or somthing</Text>
         </Flex>
       </Router>
     </ChakraProvider>
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,22 +1,54 @@
-import { Flex, Link } from '@chakra-ui/react'
+import { Flex, Link, Button } from '@chakra-ui/react'
 import { Link as RouteLink } from 'react-router-dom'
 import React from 'react'
 
 export const Navigation = () => {
   return (
-    <Flex flexDirection="row" color="darkpurple">
-      <Link as={RouteLink} to="/" ml="3rem">
+    <Flex
+      flexDirection="row"
+      bg="blue"
+      color="darkpurple"
+      justifyContent="space-evenly"
+    >
+      <Button as={RouteLink} to="/" ml="3rem" borderBottomRadius="0" bg="cyan">
         Home
-      </Link>
-      <Link as={RouteLink} to="/history" ml="3rem">
-        History
-      </Link>
-      <Link as={RouteLink} to="/contact" ml="3rem">
+      </Button>
+      <Button
+        as={RouteLink}
+        to="/account"
+        ml="3rem"
+        borderBottomRadius="0"
+        bg="cyan"
+      >
+        Account
+      </Button>
+      <Button
+        as={RouteLink}
+        to="/contact"
+        ml="3rem"
+        borderBottomRadius="0"
+        bg="cyan"
+      >
         Contact
-      </Link>
-      <Link as={RouteLink} to="/faq" ml="3rem">
+      </Button>
+      <Button
+        as={RouteLink}
+        to="/faq"
+        ml="3rem"
+        borderBottomRadius="0"
+        bg="cyan"
+      >
         FAQ
-      </Link>
+      </Button>
+      <Button
+        as={RouteLink}
+        to="/about"
+        ml="3rem"
+        borderBottomRadius="0"
+        bg="cyan"
+      >
+        About
+      </Button>
     </Flex>
   )
 }
