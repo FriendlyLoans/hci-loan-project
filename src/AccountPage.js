3e4ffc18a27b30d9d47cdb57a2030e8932117214
AJ <55856227+monkeypunk44@users.noreply.github.com>
2021-10-29 23:59:43 -0300

    AccountPage Update

--- src/AccountPage.js
+++ src/AccountPage.js
@@ -1,4 +1,4 @@
-import { Flex, Text, Heading } from '@chakra-ui/react'
+import { Flex, Text, Heading, Button } from '@chakra-ui/react'
 import React from 'react'
 import { Trans } from '@lingui/macro'
 
@@ -8,6 +8,10 @@ export const AccountPage = () => {
       <Heading mx="auto" my="2rem">
         <Trans>Account Page</Trans>
       </Heading>
+      <Button mx="10rem">
+        <Trans>Pay Here</Trans>
+      </Button>
+
       <Text m="auto" display="block">
         <Trans>This is some fresshhhhhh stuff</Trans>
       </Text>
