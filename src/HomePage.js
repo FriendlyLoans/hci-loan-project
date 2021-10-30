12675beaa7d7166d24e88a2c9c7a46a9519ad305
Kyle Sullivan <141261s@acadiau.ca>
2021-10-30 17:45:20 -0300

    Add image on home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,11 @@
-import { Flex, Text, Heading, Box } from '@chakra-ui/react'
+import { Flex, Text, Heading, Box, Image } from '@chakra-ui/react'
 import React from 'react'
-import { Trans } from '@lingui/macro'
+import { t, Trans } from '@lingui/macro'
+import graduationImage from './images/Graduation.svg'
 
 export const HomePage = () => {
   return (
-    <Flex minH="30rem" width="100%" flexDirection="column">
+    <Flex minH="30rem" width="100%" flexDirection="column" grow={1}>
       <Heading mx="auto" my="2rem">
         <Trans>Welcome to Friendly Loans!</Trans>
       </Heading>
@@ -13,12 +14,30 @@ export const HomePage = () => {
       </Text>
 
       <Text mx="auto" fontSize="1rem">
-      Friendly Loans is a service for all students to access their student loans in Nova Scotia.
+        Friendly Loans is a service for all students to access their student
+        loans in Nova Scotia.
       </Text>
 
       <Text mx="auto">
-        <Trans>Canada Students loans are managed by the  <Text as="u" _hover={{color:"teal"}}> <a href="https://www.csnpe-nslsc.canada.ca/en/home" >National Student Loans Service Centre (NSLSC). </a> </Text> </Trans>
+        <Trans>
+          Canada Students loans are managed by the{' '}
+          <Text as="u" _hover={{ color: 'teal' }}>
+            {' '}
+            <a href="https://www.csnpe-nslsc.canada.ca/en/home">
+              National Student Loans Service Centre (NSLSC).{' '}
+            </a>{' '}
+          </Text>{' '}
+        </Trans>
       </Text>
+
+      <Image
+        src={graduationImage}
+        alt={t`Student receiving their diploma`}
+        opacity="0.8"
+        maxW="calc(80%)"
+        m="auto"
+        display="block"
+      />
     </Flex>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,18 @@ module.exports = {
         },
       },
       {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
         test: /\.css$/,
         use: [
           {
