6d0ad7fcfb0be0aa6a482a5afeeed4f5ad91005c
kadclakshan <chamath.lakshan15@gmail.com>
2021-11-01 09:17:26 -0300

    Update (HomePage)

--- src/HomePage.js
+++ src/HomePage.js
@@ -14,8 +14,10 @@ export const HomePage = () => {
       </Text>
 
       <Text mx="auto" fontSize="1rem">
+        <Trans>
         Friendly Loans is a service for all students to access their student
         loans in Nova Scotia.
+        </Trans>
       </Text>
 
       <Text mx="auto">
