9759dd006c525d8d2640bc650e75a8d365d46bd4
kadclakshan <chamath.lakshan15@gmail.com>
2021-11-03 18:27:20 -0300

    Update FaqPage.js

--- src/FaqPage.js
+++ src/FaqPage.js
@@ -20,30 +20,111 @@ export const FaqPage = () => {
       </Heading>
       <Text>
         <Accordion allowMultiple>
+          {/* Question 1 */}
+          <AccordionItem mx="auto" width="75%">
+            <h2>
+              <AccordionButton _expanded={{ bg: '#E0FBFC', color: 'black' }}>
+                <Box flex="1" textAlign="left" padding="1.5rem">
+                  <Text fontSize="1.5rem">What information do I need to create an account?</Text>
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              You will need some basic information to use Friendly Loans:
+              <Text my="2rem">
+              <b>First and Last Names</b> (as it appears on your Identification Document)
+              </Text>
+
+              <Text my="2rem">
+              <b>Email Address</b> (please use a secure email address you use frequently)
+              </Text>
+
+              <Text fontWeight="bold">
+              Account Number
+              </Text>
+
+              <Text>
+              (This should be at the top right corner of the loan statement you received in the mail.)
+              </Text>
+
+              <Text>
+              If you have not received your loan statement, please contact <b><a href="tel: 1800-IDK-SOME">(+1) 800-IDK-SOME</a></b>
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+
+          {/* Question 2 */}
           <AccordionItem mx="auto" width="75%">
             <h2>
               <AccordionButton _expanded={{ bg: '#E0FBFC', color: 'black' }}>
                 <Box flex="1" textAlign="left" padding="1.5rem">
                   <Trans>
-                    <Text fontSize="1.5rem">Question 1</Text>
+                    <Text fontSize="1.5rem">I forgot my password. What now?</Text>
                   </Trans>
                 </Box>
                 <AccordionIcon />
               </AccordionButton>
             </h2>
             <AccordionPanel>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
-              voluptates architecto inventore soluta! Necessitatibus doloribus,
-              reprehenderit eos officiis labore soluta modi ad facilis non
-              consequatur quibusdam, ex quaerat optio ipsam.
+              Unfortunately, you will have to contact our customer support centre. Reach out at Customer Support Centre at:
+              <Text py="2em" fontWeight="bold">
+              <a href="mailto: example@example.ca">example@example.ca</a>
+              </Text>
+
+              <Text fontWeight="bold">
+              <a href="tel: 1800-IDK-SOME">(+1) 800-IDK-SOME</a>
+              </Text>
+
+              <Text>
+                (Available from 9:00 AM to 10:00 PM - Monday to Friday)
+              </Text>
+
+              <Text my="0.75rem">
+                Times listed are in Atlantic Daylight Time.
+              </Text>
+
+              <Text>
+                Having any information related to your account will help us resolve your issue immediately. We will still guide you in the event you are unable to present them.
+              </Text>
             </AccordionPanel>
           </AccordionItem>
 
+
+          {/* Question 3 */}
           <AccordionItem mx="auto" width="75%">
             <h2>
               <AccordionButton _expanded={{ bg: '#E0FBFC', color: 'black' }}>
                 <Box flex="1" textAlign="left" padding="1.5rem">
-                  <Text fontSize="1.5rem">Question 2</Text>
+                  <Text fontSize="1.5rem">My credentials were stolen. What should I do?</Text>
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              It is also your responsibility to keep account credentials secure. Never share this information with anyone.
+
+              <Text my="0.5rem">
+              If you believe your Account Details were stolen, please contact <b><a href="tel: (+1) 800-YOU-LOST">Friendly Loans Identity Theft Report Center</a></b>and local law enforcement authorities.
+              </Text>
+
+              <Text>
+                Friendly Loans Customer Support
+              </Text>
+
+              <Text fontWeight="bolded">
+              <a href="tel: 1800-YOU-LOST"><b>(+1) 800-YOU-LOST</b></a>
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+
+
+          {/* Question 4 */}
+          <AccordionItem mx="auto" width="75%">
+            <h2>
+              <AccordionButton _expanded={{ bg: '#E0FBFC', color: 'black' }}>
+                <Box flex="1" textAlign="left" padding="1.5rem">
+                  <Text fontSize="1.5rem"></Text>
                 </Box>
                 <AccordionIcon />
               </AccordionButton>
@@ -56,39 +137,6 @@ export const FaqPage = () => {
             </AccordionPanel>
           </AccordionItem>
 
-          <AccordionItem mx="auto" width="75%">
-            <h2>
-              <AccordionButton _expanded={{ bg: '#E0FBFC', color: 'black' }}>
-                <Box flex="1" textAlign="left" padding="1.5rem">
-                  <Text fontSize="1.5rem">Question 3</Text>
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
-              voluptates architecto inventore soluta! Necessitatibus doloribus,
-              reprehenderit eos officiis labore soluta modi ad facilis non
-              consequatur quibusdam, ex quaerat optio ipsam.
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem mx="auto" width="75%">
-            <h2>
-              <AccordionButton _expanded={{ bg: '#E0FBFC', color: 'black' }}>
-                <Box flex="1" textAlign="left" padding="1.5rem">
-                  <Text fontSize="1.5rem">Question 4</Text>
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
-              voluptates architecto inventore soluta! Necessitatibus doloribus,
-              reprehenderit eos officiis labore soluta modi ad facilis non
-              consequatur quibusdam, ex quaerat optio ipsam.
-            </AccordionPanel>
-          </AccordionItem>
 
           <AccordionItem mx="auto" width="75%">
             <h2>
@@ -106,6 +154,9 @@ export const FaqPage = () => {
               consequatur quibusdam, ex quaerat optio ipsam.
             </AccordionPanel>
           </AccordionItem>
+
+
+
         </Accordion>
       </Text>
     </Flex>
