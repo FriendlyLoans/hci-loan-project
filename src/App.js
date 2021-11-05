671576526e5f6f22505f25197e7691974c054919
Kyle Sullivan <141261s@acadiau.ca>
2021-11-04 21:58:36 -0300

    Maybe this font size?

--- src/App.js
+++ src/App.js
@@ -25,7 +25,11 @@ const App = () => {
       <ChakraProvider theme={theme}>
         <I18nProvider i18n={i18n}>
           <Router basename="/">
-            <Flex bg="gray.100" paddingLeft="calc(100vw - 100%)">
+            <Flex
+              bg="gray.100"
+              paddingLeft="calc(100vw - 100%)"
+              fontSize={{ base: '3xl', sm: '2xl', md: '2xl' }}
+            >
               <Flex
                 maxW={{
                   sm: '100%',
