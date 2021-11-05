5f51dec333b187c28ddb0be45113f0e61836974b
Kyle Sullivan <141261s@acadiau.ca>
2021-11-05 01:14:53 -0300

    Translations

--- src/PaymentPage.js
+++ src/PaymentPage.js
@@ -17,6 +17,8 @@ import {
   Tab,
   FormLabel,
   FormControl,
+  NumberInputField,
+  NumberInput,
 } from '@chakra-ui/react'
 import { ChevronDownIcon } from '@chakra-ui/icons'
 
@@ -27,6 +29,7 @@ import { Confirm } from './PaymentConf'
 export const PaymentPage = () => {
   const [currentPaymentState, setCurrentPaymentState] = useState('card')
   const [currentCardState, setCurrentCardState] = useState('existing')
+  const [currentBankState, setCurrentBankState] = useState('existing')
 
   const cardPayment = (
     <Flex w="100%" flexDirection="column">
@@ -73,7 +76,9 @@ export const PaymentPage = () => {
             mx="auto"
           >
             <FormControl id="number">
-              <FormLabel htmlFor="number">Card number</FormLabel>
+              <FormLabel htmlFor="number">
+                <Trans>Card number</Trans>
+              </FormLabel>
               <Input
                 name="number"
                 placeholder={t`Card number`}
@@ -84,7 +89,9 @@ export const PaymentPage = () => {
             </FormControl>
 
             <FormControl id="name">
-              <FormLabel htmlFor="name">Name on card</FormLabel>
+              <FormLabel htmlFor="name">
+                <Trans>Name on card</Trans>
+              </FormLabel>
               <Input
                 name="name"
                 placeholder={t`Name on card`}
@@ -95,7 +102,9 @@ export const PaymentPage = () => {
             </FormControl>
 
             <FormControl id="cvv">
-              <FormLabel htmlFor="cvv">CVV</FormLabel>
+              <FormLabel htmlFor="cvv">
+                <Trans>CVV</Trans>
+              </FormLabel>
               <Input name="cvv" placeholder={t`CVV`} w="10ch" mb="1rem" />
             </FormControl>
           </Flex>
@@ -104,8 +113,95 @@ export const PaymentPage = () => {
     </Flex>
   )
 
-  const bankPayment = <Flex>BANK</Flex>
+  const bankPayment = (
+    <Flex w="100%" flexDirection="column">
+      <Text px="2rem" mx="auto">
+        <Trans>Pay with existing bank account or add new</Trans>
+      </Text>
+      <RadioGroup
+        value={currentBankState}
+        onChange={(e) => {
+          setCurrentBankState(e)
+        }}
+        pl="2rem"
+        mb="2rem"
+        mx="auto"
+      >
+        <Stack spacing={5} direction="row">
+          <Radio colorScheme="blue" value="existing">
+            <Trans>Existing</Trans>
+          </Radio>
+          <Radio colorScheme="blue" value="new">
+            <Trans>New</Trans>
+          </Radio>
+        </Stack>
+      </RadioGroup>
 
+      {currentBankState === 'existing' ? (
+        <Flex
+          direction="column"
+          alignItems="center"
+          px="2rem"
+          mx="auto"
+          mb="2rem"
+        >
+          <Select name="selectBank" placeholder={t`Select card`}>
+            <option value="bank1">
+              <Trans>RBC *9999</Trans>
+            </option>
+          </Select>
+        </Flex>
+      ) : (
+        <form>
+          <Flex
+            flexDirection="column"
+            alignItems="left"
+            w="min-content"
+            mx="auto"
+          >
+            <FormControl id="transit">
+              <FormLabel htmlFor="transit">
+                <Trans>Transit number</Trans>
+              </FormLabel>
+              <Input
+                name="transit"
+                placeholder={t`Transit number`}
+                maxW="95%"
+                w="40ch"
+                mb="1rem"
+              />
+            </FormControl>
+
+            <FormControl id="institution">
+              <FormLabel htmlFor="name">
+                <Trans>Institution number</Trans>
+              </FormLabel>
+              <Input
+                name="institution"
+                placeholder={t`Institution number`}
+                maxW="95%"
+                w="40ch"
+                mb="1rem"
+              />
+            </FormControl>
+
+            <FormControl id="account">
+              <FormLabel htmlFor="cvv">
+                <Trans>Account number</Trans>
+              </FormLabel>
+              <Input
+                name="account"
+                placeholder={t`Account number`}
+                w="40ch"
+                maxW="95%"
+                mb="1rem"
+              />
+            </FormControl>
+          </Flex>
+        </form>
+      )}
+    </Flex>
+  )
   return (
     <Flex minH="30rem" width="100%" flexDirection="column">
       <Heading mx="auto" my="2rem">
@@ -113,7 +209,7 @@ export const PaymentPage = () => {
       </Heading>
       <Flex flexDirection="column">
         <Text textAlign="center" pl="2rem">
-          Select your payment method
+          <Trans>Select your payment method</Trans>
         </Text>
         <RadioGroup
           mx="auto"
@@ -144,13 +240,16 @@ export const PaymentPage = () => {
           <form>
             <FormControl id="paymentAmount">
               <FormLabel>Payment amount</FormLabel>
-              <Input
-                w="40ch"
-                maxW="100%"
-                name="paymentAmount"
-                placeholder="Payment amount"
-                mb="2rem"
-              />
+              <NumberInput>
+                <NumberInputField
+                  w="40ch"
+                  maxW="100%"
+                  name="paymentAmount"
+                  placeholder="Payment amount"
+                  mb="2rem"
+                  type="number"
+                />
+              </NumberInput>
             </FormControl>
           </form>
         </Flex>
