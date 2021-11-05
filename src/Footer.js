dab070944e2667f6be24591a865a2678be20a9c3
kadclakshan <chamath.lakshan15@gmail.com>
2021-11-04 22:43:56 -0300

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <Flex w="100%" bg="themeBlue" minH="5vh" textColor="themeYellow" mt="auto">
       <Text m="auto">
-        <Trans>Copyright© all right reserved for Friendly Loans</Trans>
+        <Trans>Copyright &copy; 2021 Friendly Loans. All rights reserved.</Trans>
       </Text>
       <Text m="auto">(+1)902-749-6454</Text>
       <Text m="auto">Help@FriendlyLoans.ca</Text>
